3d89524b4695567969991aa8b5c69452bdd56ae8
Alexandre Atoji <alexandre.atoji@gmail.com>
2010-11-15 17:47:55 -0200

    managing dependencies

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ setup(name='restfulie',
       version='0.1',
       namespace_packages = ['restfulie'],
       test_suite = "nose.collector",
-      setup_requires=['nose>=0.11', 'mockito>=0.5.1'],
+      install_requires= ['httplib2>=0.6.0'],
+      setup_requires=['nose>=0.11'],
+      tests_require=[ 'mockito>=0.5.1'],
       packages=['restfulie'])
 
