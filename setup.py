6c4d927b605dc34daa913890d65ebf6a7e577a63
Pedro Matiello <pmatiello@gmail.com>
2011-02-17 11:37:21 -0200

    Fixing installation problems when using pip instead of easy_install.

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ from setuptools import setup
 
 setup(name='restfulie',
       version='0.8',
-      namespace_packages = ['restfulie'],
       test_suite = "nose.collector",
       install_requires= ['httplib2>=0.6.0'],
       setup_requires=['nose>=0.11'],
