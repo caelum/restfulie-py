067793ff446c98e2aaebb0a6ffadf33088ca8250
Pedro Matiello <pmatiello@gmail.com>
2011-02-23 22:44:36 -0300

    Using more should and less test on converters_test.

--- test/converters_test.py
+++ test/converters_test.py
@@ -5,13 +5,13 @@ class converters_test:
     def setup(self):
         Converters.types = {}
 
-    def test_register(self):
+    def should_register_converters(self):
         converter = PlainConverter()
         Converters.register("text/plain", converter)
 
         assert Converters.types["text/plain"] == converter
 
-    def test_marshaller_for(self):
+    def test_should_discover_a_marshaller_for_a_type(self):
         assert Converters.marshaller_for("application/atom").__class__ == XmlConverter().__class__
         converter = PlainConverter()
         Converters.register("text/plain", converter)
@@ -19,25 +19,25 @@ class converters_test:
 
 class generic_marshaller_test:
 
-    def test_marshal(self):
+    def should_marshal(self):
         converter = PlainConverter()
         result = converter.marshal("Hello World")
         assert result == "Hello World"
 
-    def test_unmarshal(self):
+    def should_unmarshal(self):
         converter = PlainConverter()
         result = converter.unmarshal("Hello World")
         assert result == "Hello World"
 
 class json_marshaller_test:
 
-    def test_marshal(self):
+    def should_marshal(self):
         converter = JsonConverter()
         d = {'a': {'c':[1,2,3]}, 'b': 2}
         result = converter.marshal(d)
         assert result == '{"a": {"c": [1, 2, 3]}, "b": 2}'
 
-    def test_unmarshal(self):
+    def should_unmarshal(self):
         converter = JsonConverter()
         json = '{"a": {"c": [1, 2, 3]}, "b": 2}'
         result = converter.unmarshal(json)
@@ -46,13 +46,13 @@ class json_marshaller_test:
 
 class xml_marshaller_test:
 
-    def test_marshal(self):
+    def should_marshal(self):
         converter = XmlConverter()
         d = {'html': {'img': ''}}
         result = converter.marshal(d)
         assert result == '<html><img /></html>'
 
-    def test_unmarshal(self):
+    def should_unmarshal(self):
         converter = XmlConverter()
         result = converter.unmarshal('<html><img><link href="http://google.com" rel="alternative" type="application/xml">A Link</link><link href="http://yahoo.com" rel="self" type="application/xml" /></img></html>')
         assert result.tag == 'html'
