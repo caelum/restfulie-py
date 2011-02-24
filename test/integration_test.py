461139d71109a29e42397d19d831fea04795e6b0
Pedro Matiello <pmatiello@gmail.com>
2011-02-23 22:46:37 -0300

    More should and less test on integration tests names.

--- test/integration_test.py
+++ test/integration_test.py
@@ -1,24 +1,26 @@
 from restfulie.restfulie import Restfulie
 from threading import Semaphore
 
-def test_simple_request():
-    body = Restfulie.at("http://localhost:20144/hello").get().body
-    assert "Response for" in body
-    assert "/hello" in body
-
-def test_async_request_will_invoke_its_callback():
+class integration_test:
     
-    barrier = Semaphore(0)
+    def should_perform_ordinary_requests(self):
+        body = Restfulie.at("http://localhost:20144/hello").get().body
+        assert "Response for" in body
+        assert "/hello" in body
     
-    def callback(response):
-        body = response.body
-        try:
-            assert "Response for" in body
-            assert "/hello" in body
-        finally:
-            barrier.release()
-    
-    Restfulie.at("http://localhost:20144/hello").async(callback).get()
-    
-    barrier.acquire()
-    
+    def should_perform_assynchronous_requests(self):
+        
+        barrier = Semaphore(0)
+        
+        def callback(response):
+            body = response.body
+            try:
+                assert "Response for" in body
+                assert "/hello" in body
+            finally:
+                barrier.release()
+        
+        Restfulie.at("http://localhost:20144/hello").async(callback).get()
+        
+        barrier.acquire()
+        
