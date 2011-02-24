444df8cb172528572e09ead7f3dacb4592db4d6f
Pedro Matiello <pmatiello@gmail.com>
2011-02-24 17:35:36 -0300

    Better testing for asynchronous requests with and without extra arguments.

--- test/request_test.py
+++ test/request_test.py
@@ -1,6 +1,7 @@
 from restfulie.dsl import Dsl
 from restfulie.request import Request
 from mockito import mock
+from threading import Semaphore
 
 class callable_mock():
     
@@ -14,16 +15,43 @@ class http_method_test:
     
     def setup(self):
         self.dsl = mock(Dsl)
-        self.method = Request(self.dsl)
+        self.request = Request(self.dsl)
 
     def should_make_synchronous_invocations_if_callback_is_not_configured(self):
         self.dsl.callback = None
-        self.method._process_flow = callable_mock()
-        self.method()
-        assert self.method._process_flow.called == 1
+        self.request._process_flow = callable_mock()
+        self.request()
+        assert self.request._process_flow.called == 1
     
     def should_make_asynchronous_invocations_if_callback_is_configured(self):
         self.dsl.callback = lambda : None
-        self.method._process_async_flow = callable_mock()
-        self.method()
-        assert self.method._process_async_flow.called == 1
+        self.request._process_async_flow = callable_mock()
+        self.request()
+        assert self.request._process_async_flow.called == 1
+    
+    def should_call_callback_function_on_asynchronous_request(self):
+        barrier = Semaphore(0)
+        
+        def callback(request):
+            barrier.release()
+        
+        self.dsl.callback = callback
+        self.dsl.callback_args = ()
+        self.request._process_flow = lambda payload : None
+        self.request()
+        
+        barrier.acquire()
+    
+    def should_call_callback_function_on_asynchronous_request_and_pass_extra_arguments(self):
+        barrier = Semaphore(0)
+        
+        def callback(request, arg1, arg2, arg3):
+            assert (arg1, arg2, arg3) == (1, 2, 3)
+            barrier.release()
+        
+        self.dsl.callback = callback
+        self.dsl.callback_args = (1, 2, 3)
+        self.request._process_flow = lambda payload : None
+        self.request()
+        
+        barrier.acquire()
