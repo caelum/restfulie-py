d05ba83bb48f748f3e0f6528d6fca2697bead854
Pedro Matiello <pmatiello@gmail.com>
2011-02-23 22:59:13 -0300

    Clearing parser test.

--- test/parser_test.py
+++ test/parser_test.py
@@ -2,13 +2,15 @@ from restfulie.parser import Parser
 
 from mockito import mock, when
 
-def test_parser_follow():
+class parser_test:
 
-    request = mock()
-    processor = mock()
-    resource = mock()
-
-    parser = Parser([processor])
-    when(processor).execute(parser, request, {}).thenReturn(resource)
-
-    assert parser.follow(request, {}) == resource
+    def should_execute_processor(self):
+    
+        request = mock()
+        processor = mock()
+        resource = mock()
+    
+        parser = Parser([processor])
+        when(processor).execute(parser, request, {}).thenReturn(resource)
+    
+        assert parser.follow(request, {}) == resource
