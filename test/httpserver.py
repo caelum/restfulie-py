9ccaac5c430b4f412da103ad6ae16287e0bdcb1a
Douglas Camata <d.camata@gmail.com>
2011-03-27 04:19:15 -0300

    including a POST in the http server to test if parameters are sent correctly.

--- test/httpserver.py
+++ test/httpserver.py
@@ -17,6 +17,16 @@ class RequestHandler(BaseHTTPRequestHandler):
                 if auth == "Basic %s" % encodestring('test:test')[:-1]:
                     self.wfile.write('worked')
 
+    def do_POST(self, *args, **kwargs):
+        content_size = int(self.headers.getheader('content-length'))
+        body = self.rfile.read(content_size)
+        self.wfile.write('This is a test.')
+        if '<action>' in body:
+            if body.split('<action>') == 'test':
+                self.wfile.write('This is a test.')
+        else:
+            self.wfile.write('Fail.')
+
 server_address = ('', 20144)
 httpd = HTTPServer(server_address, RequestHandler)
 try:
