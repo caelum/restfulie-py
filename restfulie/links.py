306c77c9731c1d7f677e9bc2833d3ca26ac62db2
Alexandre Atoji <alexandre.atoji@gmail.com>
2011-04-16 21:30:43 -0300

    Some documentation on link.py

--- restfulie/links.py
+++ restfulie/links.py
@@ -2,6 +2,9 @@ import dsl
 
 
 class Link(object):
+    """
+    Link represents generic link. You can follow it.
+    """
 
     def __init__(self, link):
         self.href = link.get('href')
@@ -9,12 +12,23 @@ class Link(object):
         self.type = link.get('type')
 
     def follow(self):
+        """
+        Return a DSL object with the Content-Type
+        set.
+        """
         return dsl.Dsl(self.href).as_(self.type)
 
 
 class Links:
+    """
+    Links a simple Link collection. There are some
+    methods to put syntax sugar
+    """
 
     def __init__(self, links):
+        """
+        Enable Links to access attributes with 'dot'
+        """
         self.links = {}
         for link in links:
             l = Link(link)
@@ -22,7 +36,15 @@ class Links:
             setattr(self, l.rel, l)
 
     def get(self, rel):
+        """
+        Checks if a Link exists. If exists returns the
+        object, else returns None
+        """
         return self.links.get(rel)
 
     def __len__(self):
+        """
+        The length of Links is the length of the links
+        dictionary inside it
+        """
         return len(self.links)
