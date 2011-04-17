7f2f799b2fa34b2710c4cf7dcf5015f3ad5df216
Alexandre Atoji <alexandre.atoji@gmail.com>
2011-04-16 22:30:41 -0300

    OpenSearch documentation

--- restfulie/opensearch.py
+++ restfulie/opensearch.py
@@ -2,16 +2,26 @@ import dsl
 
 
 class OpenSearchDescription(object):
+    """
+    OpenSearchDescription object wraps the OpenSearch logic
+    """
 
     def __init__(self, element_tree):
         self.url_type = 'application/rss+xml'
         self.element_tree = element_tree
 
     def use(self, url_type):
+        """
+        Set the OpenSearch type
+        """
         self.url_type = url_type
         return self
 
     def search(self, searchTerms, startPage):
+        """
+        Make a search with 'searchTerms'
+        It will find the url_type URL that you have chosen
+        """
         tag = '{http://a9.com/-/spec/opensearch/1.1/}Url'
         urls = self.element_tree.findall(tag)
         for url in urls:
